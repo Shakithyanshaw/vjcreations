1bf98ff2bdcd707cf424c054a76734d0694c2736
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2024-04-11 14:59:25 +0530

    start

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">VJCREATIONS</a>
+      </header>
+      <main>Featured Products</main>
+    </div>
+  );
+}
+
+export default App;
