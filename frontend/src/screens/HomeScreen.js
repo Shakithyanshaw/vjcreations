6620f81f8344658c008cd36bf0fcd366c0d346b2
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2024-04-14 01:55:54 +0530

    create utils

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,10 +6,12 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
 import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'FETCH_REQUESR':
+    case 'FETCH_REQUEST':
       return { ...state, loading: true };
     case 'FETCH_SUCCESS':
       return { ...state, products: action.payload, loading: false };
@@ -34,7 +36,7 @@ function HomeScreen() {
         const result = await axios.get(`/api/products`);
         dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (err) {
-        dispatch({ type: 'FETCH_FAIL', palload: err.message });
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
       }
 
       //setProducts(result.data);
@@ -49,9 +51,9 @@ function HomeScreen() {
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
-          <div>Loading..</div>
+          <LoadingBox />
         ) : error ? (
-          <div>{error}</div>
+          <MessageBox variant="danger">{error}</MessageBox>
         ) : (
           <Row>
             {products.map((product) => (
