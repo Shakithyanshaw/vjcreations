19d062548ad88d9381d5c5edf93c8caa3b4b2f4f
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2024-04-24 01:09:30 +0530

    oder screen details

---
+++ frontend/src/screens/OrderScreen.js
@@ -0,0 +1,192 @@
+import React, { useContext, useEffect, useReducer } from 'react';
+import MessageBox from '../components/MessageBox';
+import LoadingBox from '../components/LoadingBox';
+import { Store } from '../Store';
+import { useNavigate, useParams } from 'react-router';
+import axios from 'axios';
+import { getError } from '../utils';
+import { Helmet } from 'react-helmet-async';
+import { Card, Col, ListGroup, Row } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, order: action.payload, error: '' };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+}
+
+export default function OrderScreen() {
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+
+  const params = useParams();
+  const { id: orderId } = params;
+  const navigate = useNavigate();
+
+  const [{ loading, error, order }, dispatch] = useReducer(reducer, {
+    loading: true,
+    order: {},
+    error: '',
+  });
+
+  useEffect(() => {
+    const fetchOrder = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/orders/${orderId}`, {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+
+    if (!userInfo) {
+      return navigate('/login');
+    }
+    if (!order._id || (order._id && order._id !== orderId)) {
+      fetchOrder();
+    }
+  }, [order, userInfo, orderId, navigate]);
+
+  return loading ? (
+    <LoadingBox></LoadingBox>
+  ) : error ? (
+    <MessageBox varient="danger">{error}</MessageBox>
+  ) : (
+    <div className="marginAll">
+      <Helmet>
+        <title>Order {orderId}</title>
+      </Helmet>
+      <h1 className="my-3">Order ID - {orderId}</h1>
+      <Row>
+        <Col md={8}>
+          <Row>
+            <Col md={6}>
+              <Card className="mb-3">
+                <Card.Body>
+                  <Card.Title>Shipping</Card.Title>
+                  <Card.Text>
+                    <strong>Name:</strong> {order.shippingAddress.fullName}{' '}
+                    <br />
+                    <strong>Address: </strong> {order.shippingAddress.address},
+                    {order.shippingAddress.city},{' '}
+                    {order.shippingAddress.postalCode},
+                    {order.shippingAddress.country}
+                  </Card.Text>
+                  <Card.Text>
+                    <strong>Event Date:</strong> {order.shippingAddress.date}{' '}
+                    <br />
+                    <strong>Event Time:</strong> {order.shippingAddress.time}{' '}
+                  </Card.Text>
+                  {order.isDelivered ? (
+                    <MessageBox variant="success">
+                      Delivered at {order.deliveredAt}
+                    </MessageBox>
+                  ) : (
+                    <MessageBox variant="danger">Not Delivered</MessageBox>
+                  )}
+                </Card.Body>
+              </Card>
+            </Col>
+
+            <Col md={6}>
+              <Card className="mb-3">
+                <Card.Body>
+                  <Card.Title>Payment</Card.Title>
+                  <Card.Text>
+                    <strong>Method:</strong> {order.paymentMethod}
+                  </Card.Text>
+                  {order.isPaid ? (
+                    <MessageBox variant="success">
+                      Paid at {order.paidAt}
+                    </MessageBox>
+                  ) : (
+                    <MessageBox variant="danger">Not Paid</MessageBox>
+                  )}
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Title>Items</Card.Title>
+              <ListGroup variant="flush">
+                {order.orderItems.map((item) => (
+                  <ListGroup.Item key={item._id}>
+                    <Row className="align-items-center">
+                      <Col md={7}>
+                        <img
+                          src={item.image}
+                          alt={item.name}
+                          className="img-fluid rounded img-thumbnail"
+                        ></img>{' '}
+                        <Link
+                          className="OrderProductName"
+                          to={`/product/${item.slug}`}
+                        >
+                          {item.name}
+                        </Link>
+                      </Col>
+                      <Col md={2}>
+                        <span>{item.quantity}</span>
+                      </Col>
+                      <Col md={3}>Rs{item.price}</Col>
+                    </Row>
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col md={4}>
+          <Card className="mb-3">
+            <Card.Body>
+              <Card.Title>Order Summary</Card.Title>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Items</Col>
+                    <Col>Rs{order.itemsPrice.toFixed(2)}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Shipping</Col>
+                    <Col>Rs{order.shippingPrice.toFixed(2)}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Tax</Col>
+                    <Col>Rs{order.taxPrice.toFixed(2)}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>
+                      <strong> Order Total</strong>
+                    </Col>
+                    <Col>
+                      <strong>Rs{order.totalPrice.toFixed(2)}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
+}
